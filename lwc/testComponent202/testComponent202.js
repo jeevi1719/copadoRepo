d1ebd40e05c1697633acd1c32a39952e5576f962
jeevi1719 <>
2025-07-17 06:46:20 +0000

    [Copado] learningOrg to github

---
+++ lwc/testComponent202/testComponent202.js
@@ -0,0 +1,13 @@
+import { LightningElement,wire, track,api } from 'lwc';
+//import { getPicklistValues } from 'lightning/uiObjectInfoApi';
+//import { getObjectInfo } from 'lightning/uiObjectInfoApi';
+
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { createRecord, updateRecord } from 'lightning/uiRecordApi';
+import Phone from '@salesforce/schema/Account.Phone';
+import { NavigationMixin } from 'lightning/navigation';
+
+export default class testComponent202 extends NavigationMixin(LightningElement) {
+
+   
+}
